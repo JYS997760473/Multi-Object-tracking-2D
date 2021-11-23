846879616cff9afaf68ea22868bf91c425f5302b
Jia Yansong <90627672+JYS997760473@users.noreply.github.com>
2021-11-23 20:49:37 +0800

    Update MOT2D_Kalman_Filter.py

--- MOT2D_Kalman_Filter.py
+++ MOT2D_Kalman_Filter.py
@@ -23,35 +23,35 @@ class KalmanTracker(object):
                               [0,0,1,0,0,0,0,0],
                               [0,0,0,1,0,0,0,0]])
 
-        self.kf.P[4:,4:] *= 1000 #速度不确定， 初始值设大
-        self.kf.P *= 10
+        self.kf.P[4:,4:] *= 1000 #速度不确定， 初始值设大   但发现并没有卵用
+        self.kf.P *= 10   #总体改进  estimate uncertainty
 
-        self.kf.Q[4:,4:] *= 0.01
+        self.kf.Q[4:,4:] *= 0.01     #process noise uncertainty
         self.kf.x[:4] = bbox2D.reshape((4,1))    #bbox2D=dets
 
         self.time_since_update = 0
         self.id = KalmanTracker.count
-        KalmanTracker.count += 1
-        self.history = []
+        KalmanTracker.count += 1        #ID
+        self.history = []               #记录这个跟踪器predict的state
         self.hits = 1  # number of total hits including the first detection
         self.hit_streak = 1  # number of continuing hit considering the first detection
         self.first_continuing_hit = 1
-        self.still_first = True
+        self.still_first = True     #给新进来的tracker初始化的标志
         self.age = 0
         self.info = info  #other info associated
 
-    def update(self,bbox2D, info):
+    def update(self,bbox2D, info):     #dim_bbox=dim_z=4   H用到了，R=None H观测矩阵
         #update the state vector with observed bbox
 
-        self.time_since_update = 0
-        self.history=[]
-        self.hits += 1
+        self.time_since_update = 0    #归为0 更新了就归位0
+        self.history=[]             #history清零
+        self.hits += 1              #记录连续跟踪次数
         self.hit_streak += 1    #number of continuing hit
         if self.still_first:
             self.first_continuing_hit += 1  #number of continuing hit in the first time
 
         #flip
-        self.kf.update(bbox2D)
+        self.kf.update(bbox2D)    #将前验估计变成后验估计
 
         self.info = info
 
@@ -59,13 +59,13 @@ class KalmanTracker(object):
         #Advances the state vector and returns the predicted bounding box estimate.
 
         self.kf.predict()
-        self.age += 1
-        if (self.time_since_update > 0):
-            self.hit_streak = 0
-            self.still_first = False
-        self.time_since_update += 1
-        self.history.append(self.kf.x)
-        return self.history[-1]   #倒数第一行
+        self.age += 1       #预测次数
+        if (self.time_since_update > 0):     # 已经预测过了
+            self.hit_streak = 0             #hit_streak 归位0  （判断只更新不预测的情况）
+            self.still_first = False     #将初始化的tracker告知已经开始预测了
+        self.time_since_update += 1      #预测次数（和update配套使用，为了去除只预测而不得到更新的跟踪器）
+        self.history.append(self.kf.x)     #往history里添加现在state
+        return self.history[-1]   #倒数第一行     #返回现在的state矩阵（就是前验估计）
 
     def get_state(self):
         # return the current bounding box estimate
