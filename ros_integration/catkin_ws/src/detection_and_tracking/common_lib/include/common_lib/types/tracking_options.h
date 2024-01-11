6d898438a31e02395accacd6ca68552995e7f9f5
JYS997760473 <jiayansong@u.nus.edu>
2024-01-11 20:42:40 +0800

    tracking

---
+++ ros_integration/catkin_ws/src/detection_and_tracking/common_lib/include/common_lib/types/tracking_options.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Eigen/Core>
+
+struct TrackingOptions {
+  TrackingOptions() = default;
+
+  
+};
---
+++ ros_integration/catkin_ws/src/detection_and_tracking/tracking_lib/include/tracking/tracking_worker_manager.hpp
@@ -0,0 +1,7 @@
+#ifndef TRACKING_TRACKING_WORKER_MANAGER_HPP_
+#define TRACKING_TRACKING_WORKER_MANAGER_HPP_
+
+namespace tracking {
+  static std::unique_ptr
+}
+#endif
